05da05e93e30b2a9bb213226e120cb2804d6612e
Aniket rathore <Aniketrathore911@gmail.com>
2024-12-28 11:43:47 +0530

    Longest Substring without repeating character

--- LongestSubstring.java
+++ LongestSubstring.java
@@ -4,6 +4,7 @@ class LongestSubstring
 {
 	public static void main(String ar[])
 	{
+		/*
        //Brute force Approach =   t.c=>o(n2), s.c=o(256)		
 		String s="abcdza";
 		int maxlen=0;
@@ -29,6 +30,30 @@ class LongestSubstring
 		}
 		System.out.println("Maximum Length is "+maxlen);
 		System.out.println("Longest Substring is " +ans);
+	*/
+	//optimal approach tc=>o(n)
+	String s="abwcdaxbaz";
+	int n=s.length();
+	int l=0;
+	int r=0;
+	int maxlen=0;
+	int hash[]=new int[256];
+	Arrays.fill(hash,-1);
+	while(r<n)
+	{
+		if(hash[s.charAt(r)]!=-1)
+		{
+			if(hash[s.charAt(r)]>=l)
+			{
+				l=hash[s.charAt(r)] +1;
+			}
+		}
+		int len=r-l+1;
+		maxlen=Math.max(len,maxlen);
+		hash[s.charAt(r)]=r;
+		r++;
+	}
+	System.out.println(maxlen);
 	}
 	
 	
